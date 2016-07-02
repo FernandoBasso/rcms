beef60421f9156107556e32a3f98a11705880071
Fernando Basso <fernandobasso.br@gmail.com>
2016-07-02 10:52:01 -0300

    Upgrade to rails 5.0. Tests updated and passing.

--- test/controllers/admin/post_categories_controller_test.rb
+++ test/controllers/admin/post_categories_controller_test.rb
@@ -1,50 +1,63 @@
 require 'test_helper'
 
-class Admin::PostCategoriesControllerTest < ActionController::TestCase
+class Admin::PostCategoriesControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     @post_category = post_categories(:one)
   end
 
   test "should get index" do
-    get :index
+    get admin_post_categories_path
     assert_response :success
-    assert_not_nil assigns(:post_categories)
+    #assert_not_nil assigns(:post_categories)
   end
 
   test "should get new" do
-    get :new
+    get new_admin_post_category_path
     assert_response :success
   end
 
   test "should create post_category" do
     assert_difference('PostCategory.count') do
-      post :create, post_category: { description: @post_category.description, slug: @post_category.slug, title: @post_category.title }
+      post admin_post_categories_url, params: {
+        post_category: {
+          description: @post_category.description,
+          slug: @post_category.slug,
+          title: @post_category.title
+        }
+      }
     end
 
-    assert_redirected_to admin_post_category_path(assigns(:post_category))
+    assert_redirected_to admin_post_category_url(PostCategory.last)
   end
 
   test "should show post_category" do
-    get :show, id: @post_category
+    get admin_post_category_url(@post_category.id)
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @post_category
+    get edit_admin_post_category_url(@post_category)
     assert_response :success
   end
 
   test "should update post_category" do
-    patch :update, id: @post_category, post_category: { description: @post_category.description, slug: @post_category.slug, title: @post_category.title }
-    assert_redirected_to admin_post_category_path(assigns(:post_category))
+    patch admin_post_category_url(@post_category), params: {
+      post_category: {
+        description: @post_category.description,
+        slug: @post_category.slug,
+        title: @post_category.title
+      }
+    }
+
+    assert_redirected_to admin_post_category_url(@post_category)
   end
 
   test "should destroy post_category" do
     assert_difference('PostCategory.count', -1) do
-      delete :destroy, id: @post_category
+      delete admin_post_category_url(@post_category)
     end
 
-    assert_redirected_to admin_post_categories_path
+    assert_redirected_to admin_post_categories_url
   end
 end
